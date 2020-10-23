e4d6330bc89122c9e1a6ba68f748fb801566e9ec
MJO7 <71820617+MJO7@users.noreply.github.com>
2020-10-23 13:16:11 +0530

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,33 @@
+class Dustbin {
+    constructor(x, y, width, height) {
+      var options = {
+          isStatic:true,
+          restitution:0.3,
+          friction:0.5,
+          density:1.2,
+      }
+      this.body = Bodies.rectangle(x, y,width,height, options);
+      this.width = width;
+      this.height = height;
+      this.image = loadImage("dustbingreen.png")
+      
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      image(this.image , 480,350,250,300)
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      fill("blue");
+      strokeWeight(5)
+      stroke("green")
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
+    
+  };
+  
